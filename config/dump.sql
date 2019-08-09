858e47dfe4b8b5e6586c731ecd9da0840ad5572f
CarlosMagnoSTavares <carloscrls@hotmail.com>
2019-08-09 16:59:37 -0300

    Sump com massa de dados
    Massa de dados para teste

--- config/dump.sql
+++ config/dump.sql
@@ -11,10 +11,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-SELECT CONCAT(column_name,':<input type="text" name="',column_name,'">') FROM information_schema.`COLUMNS` 
-WHERE table_schema = 'lar_feliz' 
-AND TABLE_NAME = 'dados_pessoais'
-ORDER BY TABLE_NAME, ordinal_position;
 
 -- Copiando estrutura do banco de dados para lar_feliz
 CREATE DATABASE IF NOT EXISTS `lar_feliz` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
@@ -31,7 +27,10 @@ CREATE TABLE IF NOT EXISTS `admin` (
   PRIMARY KEY (`id_admin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.admin: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela lar_feliz.atividade
 CREATE TABLE IF NOT EXISTS `atividade` (
   `id_atividade` int(11) NOT NULL AUTO_INCREMENT,
@@ -43,7 +42,10 @@ CREATE TABLE IF NOT EXISTS `atividade` (
   PRIMARY KEY (`id_atividade`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.atividade: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `atividade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `atividade` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela lar_feliz.dados_pessoais
 CREATE TABLE IF NOT EXISTS `dados_pessoais` (
   `id_pessoal` int(11) NOT NULL AUTO_INCREMENT,
@@ -69,9 +71,78 @@ CREATE TABLE IF NOT EXISTS `dados_pessoais` (
   PRIMARY KEY (`id_pessoal`),
   KEY `fk_filiacao_desligamento` (`fk_id_filiacao_desligamento`),
   CONSTRAINT `fk_filiacao_desligamento` FOREIGN KEY (`fk_id_filiacao_desligamento`) REFERENCES `filiacao` (`id_filiacao`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.dados_pessoais: ~65 rows (aproximadamente)
+/*!40000 ALTER TABLE `dados_pessoais` DISABLE KEYS */;
+REPLACE INTO `dados_pessoais` (`id_pessoal`, `nome`, `caminho_foto`, `endereco`, `data_acolhimento`, `motivo_acolhimento`, `anexo_certidao`, `anexo_CPF`, `anexo_cartao_cidadao`, `anexo_carteira_vacinacao`, `anexo_guia_recolhimento`, `anexo_determinacao_acolhimento`, `anexo_historico_escolar`, `dados_bancarios`, `tipo_sanguineo`, `aspectos_gerais_obs`, `visitas_familiares_obs`, `data_desligamento`, `fk_id_filiacao_desligamento`, `update_at`) VALUES
+  (1, 'Carlos Magno Silva Tavares', 'css/perfil.png', NULL, '2019-08-07 10:23:59', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 10:31:43'),
+  (2, 'João Marcos Costa Gonçalves', 'css/perfil.png', NULL, '2019-08-07 10:32:51', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 10:32:56'),
+  (3, 'Thales Silva Brandin', 'css/perfil.png', NULL, '2019-08-07 10:35:11', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 10:35:15'),
+  (4, 'João Marcos Costa', 'css/perfil.png', NULL, '2019-08-07 10:35:51', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 10:35:53'),
+  (5, 'Marcio Joaquim Pereira', 'css/perfil.png', NULL, '2019-08-07 10:35:54', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 10:35:55'),
+  (6, 'Teste nome 6', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (7, 'Teste nome 7', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (8, 'Teste nome 8', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (9, 'Teste nome 9', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (10, 'Teste nome 10', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (11, 'Teste nome 11', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (12, 'Teste nome 12', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (13, 'Teste nome 13', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (14, 'Teste nome 14', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (15, 'Teste nome 15', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (16, 'Teste nome 16', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (17, 'Teste nome 17', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (18, 'Teste nome 18', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (19, 'Teste nome 19', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (20, 'Teste nome 20', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (21, 'Teste nome 21', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (22, 'Teste nome 22', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (23, 'Teste nome 23', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (24, 'Teste nome 24', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (25, 'Teste nome 25', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (26, 'Teste nome 26', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (27, 'Teste nome 27', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (28, 'Teste nome 28', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (29, 'Teste nome 29', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (30, 'Teste nome 30', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (31, 'Teste nome 31', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (32, 'Teste nome 32', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (33, 'Teste nome 33', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (34, 'Teste nome 34', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (35, 'Teste nome 35', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (36, 'Teste nome 36', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (37, 'Teste nome 37', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (38, 'Teste nome 38', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (39, 'Teste nome 39', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (40, 'Teste nome 40', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (41, 'Teste nome 41', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (42, 'Teste nome 42', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (43, 'Teste nome 43', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (44, 'Teste nome 44', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (45, 'Teste nome 45', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (46, 'Teste nome 46', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (47, 'Teste nome 47', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (48, 'Teste nome 48', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (49, 'Teste nome 49', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (50, 'Teste nome 50', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (51, 'Teste nome 51', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (52, 'Teste nome 52', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (53, 'Teste nome 53', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (54, 'Teste nome 54', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (55, 'Teste nome 55', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (56, 'Teste nome 56', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (57, 'Teste nome 57', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (58, 'Teste nome 58', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (59, 'Teste nome 59', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (60, 'Teste nome 60', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (61, 'Teste nome 61', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (62, 'Teste nome 62', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (63, 'Teste nome 63', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (64, 'Teste nome 64', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 17:56:38'),
+  (65, '65 Teste', 'css/perfil.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-08-07 18:09:27');
+/*!40000 ALTER TABLE `dados_pessoais` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela lar_feliz.educacao
 CREATE TABLE IF NOT EXISTS `educacao` (
   `id_educacao` int(11) NOT NULL AUTO_INCREMENT,
@@ -89,7 +160,10 @@ CREATE TABLE IF NOT EXISTS `educacao` (
   PRIMARY KEY (`id_educacao`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.educacao: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `educacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `educacao` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela lar_feliz.filiacao
 CREATE TABLE IF NOT EXISTS `filiacao` (
   `id_filiacao` int(11) NOT NULL AUTO_INCREMENT,
@@ -106,7 +180,10 @@ CREATE TABLE IF NOT EXISTS `filiacao` (
   CONSTRAINT `fk_ref_dados_pessoais` FOREIGN KEY (`fk_id_pessoal`) REFERENCES `dados_pessoais` (`id_pessoal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.filiacao: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `filiacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `filiacao` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela lar_feliz.ocorrencia
 CREATE TABLE IF NOT EXISTS `ocorrencia` (
   `id_ocorrencia` int(11) NOT NULL AUTO_INCREMENT,
@@ -120,10 +197,14 @@ CREATE TABLE IF NOT EXISTS `ocorrencia` (
   PRIMARY KEY (`id_ocorrencia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.ocorrencia: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `ocorrencia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ocorrencia` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela lar_feliz.registro_tecnico
 CREATE TABLE IF NOT EXISTS `registro_tecnico` (
   `id_registro_tecnico` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_id_pessoal` int(11) DEFAULT NULL,
   `fk_id_filiacao_visita` int(11) DEFAULT NULL,
   `visita_domiciliar` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `informacoes_sobre_visita` varchar(255) COLLATE utf8_bin DEFAULT NULL,
@@ -134,7 +215,10 @@ CREATE TABLE IF NOT EXISTS `registro_tecnico` (
   PRIMARY KEY (`id_registro_tecnico`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.registro_tecnico: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `registro_tecnico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registro_tecnico` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela lar_feliz.saude
 CREATE TABLE IF NOT EXISTS `saude` (
   `id_saude` int(11) NOT NULL AUTO_INCREMENT,
@@ -152,7 +236,10 @@ CREATE TABLE IF NOT EXISTS `saude` (
   CONSTRAINT `fk_saude_dados_pessoais` FOREIGN KEY (`fk_id_pessoal`) REFERENCES `dados_pessoais` (`id_pessoal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lar_feliz.saude: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `saude` DISABLE KEYS */;
+/*!40000 ALTER TABLE `saude` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
