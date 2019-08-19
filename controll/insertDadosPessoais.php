ee3a0255845b267e9592e2c106852cbba150a9ca
Carlos Tavares <carloscrls@hotmail.com>
2019-08-19 11:32:03 -0300

    Persistencia dos anexo
    mantem a persistencia dos anexos ao dar um UPDATE sem alterar nenhum anexo

--- controll/insertDadosPessoais.php
+++ controll/insertDadosPessoais.php
@@ -1,7 +1,9 @@
 <?php 
 require_once('Crud.class.php');
 
-$acao = isset($_POST['salvar'])? $_POST['salvar']:"vazio";
+$acao = isset($_POST['salvar']) && !empty($_POST['salvar'])? $_POST['salvar']:"vazio";
+$table = 'dados_pessoais';
+$telaRedirect="dadosPessoais.php";
 
 
 if (!empty($_POST)) {
@@ -19,15 +21,50 @@ if (!empty($_POST)) {
 	$aspectos_gerais_obs = !empty($_POST['aspectos_gerais_obs'])? $_POST['aspectos_gerais_obs'] : NULL;
 	$visitas_familiares_obs = !empty($_POST['visitas_familiares_obs'])? $_POST['visitas_familiares_obs'] : NULL;
 	
+
 	//Guarda anexo vindo do POST na pasta e pega o caminho como resposta para salvar no banco↓
-	$foto = isset($_FILES['foto'])? $crud->saveFile("foto",$_FILES['foto']): NULL;
-	$anexo_certidao = isset($_FILES['anexo_certidao'])? $crud->saveFile("anexo_certidao",$_FILES['anexo_certidao']): NULL;
-	$anexo_CPF = isset($_FILES['anexo_CPF'])? $crud->saveFile("anexo_CPF",$_FILES['anexo_CPF']): NULL;
-	$anexo_cartao_cidadao = isset($_FILES['anexo_cartao_cidadao'])? $crud->saveFile("anexo_cartao_cidadao",$_FILES['anexo_cartao_cidadao']): NULL;
-	$anexo_carteira_vacinacao = isset($_FILES['anexo_carteira_vacinacao'])? $crud->saveFile("anexo_carteira_vacinacao",$_FILES['anexo_carteira_vacinacao']): NULL;
-	$anexo_guia_recolhimento = isset($_FILES['anexo_guia_recolhimento'])? $crud->saveFile("anexo_guia_recolhimento",$_FILES['anexo_guia_recolhimento']): NULL;
-	$anexo_determinacao_acolhimento = isset($_FILES['anexo_determinacao_acolhimento'])? $crud->saveFile("anexo_determinacao_acolhimento",$_FILES['anexo_determinacao_acolhimento']): NULL;
-	$anexo_historico_escolar = isset($_FILES['anexo_historico_escolar'])? $crud->saveFile("anexo_historico_escolar",$_FILES['anexo_historico_escolar']): NULL;
+	$foto = isset($_FILES['foto']) && $_FILES['foto']['size'] > 0 ? 
+			$crud->saveFile("foto", $_FILES['foto']): "zero";
+
+	$anexo_certidao = isset($_FILES['anexo_certidao']) && $_FILES['anexo_certidao']['size'] > 0 ? 
+			$crud->saveFile("anexo_certidao", $_FILES['anexo_certidao']): "zero";
+
+	$anexo_CPF = isset($_FILES['anexo_CPF']) && $_FILES['anexo_CPF']['size'] > 0 ? 
+			$crud->saveFile("anexo_CPF", $_FILES['anexo_CPF']): "zero";
+
+	$anexo_cartao_cidadao = isset($_FILES['anexo_cartao_cidadao']) && $_FILES['anexo_cartao_cidadao']['size'] > 0 ? 
+			$crud->saveFile("anexo_cartao_cidadao", $_FILES['anexo_cartao_cidadao']): "zero";
+
+	$anexo_carteira_vacinacao = isset($_FILES['anexo_carteira_vacinacao']) && $_FILES['anexo_carteira_vacinacao']['size'] > 0 ? 
+			$crud->saveFile("anexo_carteira_vacinacao", $_FILES['anexo_carteira_vacinacao']): "zero";
+
+	$anexo_guia_recolhimento = isset($_FILES['anexo_guia_recolhimento']) && $_FILES['anexo_guia_recolhimento']['size'] > 0 ? 
+			$crud->saveFile("anexo_guia_recolhimento", $_FILES['anexo_guia_recolhimento']): "zero";
+
+	$anexo_determinacao_acolhimento = isset($_FILES['anexo_determinacao_acolhimento']) && $_FILES['anexo_determinacao_acolhimento']['size'] > 0 ? 
+			$crud->saveFile("anexo_determinacao_acolhimento", $_FILES['anexo_determinacao_acolhimento']): "zero";
+
+	$anexo_historico_escolar = isset($_FILES['anexo_historico_escolar']) && $_FILES['anexo_historico_escolar']['size'] > 0 ? 
+			$crud->saveFile("anexo_historico_escolar", $_FILES['anexo_historico_escolar']): "zero";
+
+
+	//Garante a persistencia dos anexos na edição (Caso não informe o anexo ele mantem o antigo)
+	if ($acao == 'editar') {
+		$where = " id = ".$_POST['id'];
+		$anexos = $crud->select($table,$where,NULL,NULL);
+		foreach ($anexos as $key => $anexo) 
+		{
+			$foto = ($foto=="zero")? $anexo['caminho_foto'] : $foto ;
+			$anexo_certidao = ($anexo_certidao=="zero")? $anexo['anexo_certidao'] : $anexo_certidao ;
+			$anexo_CPF = ($anexo_CPF=="zero")? $anexo['anexo_CPF'] : $anexo_CPF ;
+			$anexo_cartao_cidadao = ($anexo_cartao_cidadao=="zero")? $anexo['anexo_cartao_cidadao'] : $anexo_cartao_cidadao ;
+			$anexo_carteira_vacinacao = ($anexo_carteira_vacinacao=="zero")? $anexo['anexo_carteira_vacinacao'] : $anexo_carteira_vacinacao ;
+			$anexo_guia_recolhimento = ($anexo_guia_recolhimento=="zero")? $anexo['anexo_guia_recolhimento'] : $anexo_guia_recolhimento ;
+			$anexo_determinacao_acolhimento = ($anexo_determinacao_acolhimento=="zero")? $anexo['anexo_determinacao_acolhimento'] : $anexo_determinacao_acolhimento ;
+			$anexo_historico_escolar = ($anexo_historico_escolar=="zero")? $anexo['anexo_historico_escolar'] : $anexo_historico_escolar ;
+		}
+	}
+
 
 	$values =  array
 	(
@@ -50,7 +87,8 @@ if (!empty($_POST)) {
 		$anexo_historico_escolar
 	);
 
-	$columns = array (
+	$columns = array 
+	(
 		'nome',
 		'endereco',
 		'data_acolhimento',
@@ -67,46 +105,30 @@ if (!empty($_POST)) {
 		'anexo_carteira_vacinacao',
 		'anexo_guia_recolhimento',
 		'anexo_determinacao_acolhimento',
-		'anexo_historico_escolar');
+		'anexo_historico_escolar'
+	);
 	
-	$table = 'dados_pessoais';
-
 
 	$count = count($columns);
 	$separador = "";
 	$setValues = "";
-	for ($i=0; $i < $count ; $i++) { 
+	for ($i=0; $i < $count ; $i++) 
+	{ 
 		$setValues .= $separador.$columns[$i]." = '".$values[$i]."' ";
 		$separador = ", ";
 	}
 
-	if ($acao =='inserir') {
+	if ($acao =='inserir') 
+	{
 		$insert = $crud->insert($table,$columns,$values);
-		header('Location:../dadosPessoais.php?start=insert_'.$insert);
+		header('Location:../'.$telaRedirect.'?start=insert_'.$insert);
 	}
-	else if ($acao == 'editar') {
+	else if ($acao == 'editar') 
+	{
 		$where = " id = ".$_POST['id'];
 		$update= $crud->update($table,$setValues,$where);
-		header('Location:../dadosPessoais.php?start=update_'.$update);
+		header('Location:../'.$telaRedirect.'?start=update_'.$update);
 	}
 	
 }
-?>
-
-
-
-
-<?php
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
